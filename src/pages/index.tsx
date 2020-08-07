13dbbfe77a870e9add2f99359028446a7f2b1916
Alex Graham <nagraham00@gmail.com>
2020-08-07 16:23:00 -0700

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+export default class HomePage extends React.Component<{}, {}> {
+
+    constructor(props: {}) {
+        super(props);
+    }
+
+    render() {
+        return <div>
+            <h1>Hello, Nextjs!</h1>
+        </div>
+    }
+}
